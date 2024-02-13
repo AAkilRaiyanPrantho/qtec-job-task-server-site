ca88e1544a82dca630190282ec37334803d210fa
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2024-02-14 00:27:44 +0600

    Added the add task functionality

--- index.js
+++ index.js
@@ -31,6 +31,14 @@ async function run() {
       res.send(result);
     })
 
+    // Posting new task to the Database
+    app.post("/allTasks", async(req, res) => {
+      const newTask = req.body;
+      console.log(newTask);
+      const result = await allTasksCollection.insertOne(newTask);
+      res.send(result);
+    });
+
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
